eedfafa67c4dc49c788e70ba150bb81c4e7341c6
Diego Lima <diegolima362@gmail.com>
2020-05-13 23:23:01 -0300

    remove unnecessary info

--- parse_data.py
+++ parse_data.py
@@ -1,12 +1,10 @@
 import time
 import requests
-import pandas as pd
 import json
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
 
 def extract_schedule(info):
     (time, local) = info.split('-')[0:2]
@@ -49,8 +47,7 @@ def save_file(data, filename):
         js = json.dumps(data, indent=4)
         jp.write(js)
 
-
-course_data = "<section class='panel'><div class='user-heading bg-primary'><h2>REDES DE COMPUTADORES</h2><p>CPT01036</p></div><ul class='nav nav-pills nav-stacked'><li><i class='fa fa-info-circle'></i>Carga horária de 60</li><li><i class='fa fa-user icone-margem-rdm'></i>DJALMA DE MELO CARVALHO FILHO</li><li><i class='fa fa-clock-o icone-margem-rdm'></i>Quarta 11:00 (2) - Sala: B103<br>Sexta 09:00 (2) - Sala: B104 - Lab.Inf do CCT<br><i class='disciplina-turno ico-sun diurno tooltips' title='' data-original-title='Diurno'></i></li><li class='col-md-12 tabela-rdm'><div class='text-center col-md-6 col-xs-6'><h5>---</h5>Unid. I</div><div class='text-center col-md-6 col-xs-6'><h5>---</h5>Unid. II</div></li><li class='col-md-12 rdm-falta-final'><div class='col-md-6 col-xs-6 rdm-falta'><div class='gauge-canvas text-center'><canvas width='85' height='60' id='gauge0'></canvas></div><div class='text-center'><span id='gauge-textfield0' class='gauge-value'>0</span><strong>/15</strong> Faltas</div></div><div class='text-center col-md-6 col-xs-6 rdm-final'><h5 class='text-blue-dark'>---</h5>Prova final</div></li></ul></section>"
+course_data = ' '
 courses = []
 courses.append(build_course(course_data))
 save_file(courses, 'courses.json')
