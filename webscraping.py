8f02b62ec9acb024c93ce78d02f664eb4b3193e4
Diego Lima <diegolima362@gmail.com>
2020-05-14 22:08:39 -0300

    finish the scraping code

--- webscraping.py
+++ webscraping.py
@@ -1,45 +1,81 @@
-import time
-import requests
-import pandas as pd
 import json
 
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
+import parse_data as parser
 
-data = {
 
-}
+def save_file(info, filename):
+    with open(filename, 'w') as jp:
+        json.dump(info, jp, ensure_ascii=False, indent=4)
 
-info = {}
 
-url = 'https://academico.uepb.edu.br/ca/index.php/usuario/autenticar'
+def load_user():
+    user_info = {'nome_usuario': input("Matricula: "), 'senha_usuario': input("Senha: ")}
+
+    return user_info
+
+
+def login():
+    user = load_user()
+
+    user_xpath = "/html/body/div/div/div/form/div[2]/div/div/input[1]"
+    pass_xpath = "/html/body/div/div/div/form/div[2]/div/div/input[2]"
+    submit_xpath = "/html/body/div/div/div/form/div[2]/div/button"
+
+    driver.find_element_by_xpath(user_xpath).send_keys(user['nome_usuario'])
+    driver.find_element_by_xpath(pass_xpath).send_keys(user['senha_usuario'])
+    driver.find_element_by_xpath(submit_xpath).click()
+
+    driver.get(url_history)
+
+    if driver.current_url != url_history:
+        print("> Login fails\n> Exiting...")
+        driver.quit()
+        exit()
+
+
+#################################################################################
+
+
+home_url = "https://academico.uepb.edu.br/ca/index.php/usuario/autenticar"
+url_history = "https://academico.uepb.edu.br/ca/index.php/alunos/historico"
+url_rdm = "https://academico.uepb.edu.br/ca/index.php/alunos/rdm"
+
+option = Options()
+option.headless = True
 
 print("> starting browser...")
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=option)
 print("> browser started")
 
 print("> getting url...")
-driver.get(url)
+driver.get(home_url)
 
 print("> login...")
-driver.find_element_by_name("nome_usuario").send_keys(data['nome_usuario'])
-driver.find_element_by_name("senha_usuario").send_keys(data['senha_usuario'])
-driver.find_element_by_css_selector("body > div > div > div > form > div.login-wrap > div > button").click()
+login()
 print("> logged ")
 
-print("> getting CRE ...")
-cre = driver.find_element_by_xpath("//*[@id='main-content']//section//div[2]//div[1]//div//div//span").get_attribute('outerHTML')
-cre_value = BeautifulSoup(cre,'html.parser').find('span').contents[0]
+profile_css_selector = "#main-content > section > section"
+courses_data_selector = "#main-content > section > div:nth-child(3)"
 
-info['cre'] = cre_value
+print("> Getting profile data...")
+profile_data = driver.find_element_by_css_selector(profile_css_selector).get_attribute("outerHTML")
 
+print("> Getting courses data...")
+driver.get(url_rdm)
+courses_data = driver.find_element_by_css_selector(courses_data_selector).get_attribute("outerHTML")
 
-# print("> loading RDM ...")
-# driver.find_element_by_css_selector("#nav-accordion > li:nth-child(3) > a").click()
+print("> Parsing data")
+data = {'profile': parser.extract_profile(profile_data), 'courses': parser.extract_courses(courses_data)}
 
+print("> Closing browser...")
 driver.quit()
+print("> Browser closed")
 
-print("> Saving file with info...")
-print("> Done!")
+print("> Saving file...")
+save_file(data, 'data.json')
+print("> Files save 'data.json'")
+
+print("> All done")
